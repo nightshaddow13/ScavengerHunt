13136b79184919a85c115009d6c494e1d47d1591
Xavier Goss <xgoss@live.com>
2024-11-03 10:28:43 -0500

    Added service projects

---
+++ ScavengerHunt.ServiceModel/Roles.cs
@@ -0,0 +1,8 @@
+namespace ScavengerHunt.ServiceModel;
+
+public class Roles
+{
+    public const string Staff = nameof(Staff);
+    public const string Camper = nameof(Camper);
+    public const string Admin = nameof(Admin);
+}
