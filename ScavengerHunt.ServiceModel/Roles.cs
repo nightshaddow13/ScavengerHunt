6e863be0b78c74e37c21f4cea31603af3bd58383
Xavier Goss <xgoss@live.com>
2024-11-03 10:23:39 -0500

    Convert to mudblazor template

--- ScavengerHunt.ServiceModel/Roles.cs
+++
@@ -1,8 +0,0 @@
-namespace ScavengerHunt.ServiceModel;
-
-public class Roles
-{
-    public const string Staff = nameof(Staff);
-    public const string Camper = nameof(Camper);
-    public const string Admin = nameof(Admin);
-}
